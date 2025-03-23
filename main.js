eda6c981d1b26bfd36b8bbba60c9806ab19513c6
Leejehyuk <44721784+dlwpgur03@users.noreply.github.com>
2025-03-23 18:52:23 +0900

    final

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
-document.getElementById('time').innerHTML = new Date().toLocaleString()
+document.getElementById('time').innerHTML = new Date().toLocaleString() //현재웹문서를 나타냄
 
 function pink() {
   document.body.style.backgroundColor = 'pink'
 }
-
-function lightblue() {
-  document.body.style.backgroundColor = 'lightblue'
+function Lightblue() {
+  document.body.style.backgroundColor = 'Lightblue'
 }
-function yellow() {
-  document.body.style.backgroundColor = 'yellow'
+function Lightyellow() {
+  document.body.style.backgroundColor = 'Lightyellow'
 }
-function reset() {
-  document.body.style.backgroundColor = 'linen'
+function Reset() {
+  document.body.style.backgroundColor = 'White'
 }
 
 function showhtml() {
-  document.getElementById('fig').src = 'img/html.png'
+  document.getElementById('fig').src = 'html1.png'
   document.getElementById('desc').innerHTML =
-    '<b>HTML</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다.'
+    '<b>HTML</b>은 구조적 웹문서를 작성하는데 사용하는 마크앱 언어이다'
 }
-
-function showcss() {
-  document.getElementById('fig').src = 'img/css.png'
+function showCSS() {
+  document.getElementById('fig').src = 'CSS.png'
   document.getElementById('desc').innerHTML =
-    '<b>CSS3</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다.'
+    '<b>CSS</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다.'
 }
-
 function showjs() {
-  document.getElementById('fig').src = 'img/JScript.png'
+  document.getElementById('fig').src = 'js.png'
   document.getElementById('desc').innerHTML =
-    '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다. '
+    '<b>js</b>는 웹을 위한 프로그래밍언어이며 웹문서에서 동작을 실행할 수 있게 된다.'
 }
 
 function hide() {
